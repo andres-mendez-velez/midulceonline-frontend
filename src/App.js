052444febe153700e5e05ae91739e24821c5bff2
andres-mendez-velez <andresmendez1220@gmail.com>
2022-11-01 08:25:53 -0500

    Adicion TablaCategorias, Banner y Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Header from "./general/Header";
+import Banner from "./general/Banner";
+import TablaCategorias from "./categorias/TablaCategorias";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div>
-      Hola mundo
+      <Header/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Banner/>} exact/>
+        <Route path="/categorias" element={<TablaCategorias/>} exact/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
