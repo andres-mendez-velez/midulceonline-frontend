8246e61dbd60a0799ae36c3624c6d7991833f8d6
andres-mendez-velez <andresmendez1220@gmail.com>
2022-11-13 11:38:10 -0500

    actualizacion frontend

--- src/componentes/general/Header.js
+++ src/componentes/general/Header.js
@@ -36,7 +36,7 @@ const Header = () => {
 
   return (
     <div>
-      <header className="fuente d-flex flex-wrap justify-content-center py-2 mb-1 border-bottom bg-light">
+      <header className="fuente d-flex flex-wrap justify-content-center py-2 mb-3 border-bottom bg-light">
         <a href="/" className="d-flex align-items-center mb-md-0 me-md-auto text-dark text-decoration-none ms-5">
           <span className="fs-4"><img src={logo} height="30" alt='Logo de MiDulceOnline' /><strong>MiDulceOnline</strong></span>
         </a>
@@ -45,12 +45,12 @@ const Header = () => {
             (
               <ul className="nav nav-pills me-5">
                 <li className="nav-item me-2">
-                  <a href="/login/form" className="nav-link active">
+                  <a href="/login/form" className="nav-link">
                     <i className="bi bi-bookmark-star me-2"></i>Ingreso
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a href="/usuarios/form" className="nav-link active">
+                  <a href="/usuarios/form" className="nav-link">
                     <i className="bi bi-bookmark-star me-2"></i>Registro
                   </a>
                 </li>
@@ -58,44 +58,47 @@ const Header = () => {
             ) :
             usuario.estadologin === EstadosLogin.ADMIN ?
               (
-                <ul style={{ "listStyle": "none" }}>
-                  <li>
-                    {usuario.nombres}
+                <ul className="nav nav-pills me-5">
+                  <li className="nav-item">
+                    <a href="/usuarios/form" className="nav-link active" aria-current="page">
+                      <i class="bi bi-person-square me-2"></i>{usuario.nombres}
+                    </a>
                   </li>
-                  <li>
-                    <ul className="nav nav-pills me-5">
-                      <li className="nav-item">
-                        <a href="/" className="nav-link active" aria-current="page">
-                          <i className="bi bi-house-heart me-2" />Inicio
-                        </a>
-                      </li>
-                      <li className="nav-item">
-                        <a href="/categorias" className="nav-link">
-                          <i className="bi bi-bookmark-star me-2" />Categorías
-                        </a>
-                      </li>
-                      <li className="nav-item">
-                        <a href="/productos" className="nav-link">
-                          <i className="bi bi-bag-heart me-2" />Productos
-                        </a>
-                      </li>
-                      <li className="nav-item">
-                        <a href="/pedidos" className="nav-link"><i className="bi bi-truck me-2" />Pedidos
-                        </a>
-                      </li>
-                      <li className="nav-item">
-                        <button type='button' onClick={cerrarSesion} className="nav-link">
-                          <i class="bi bi-x-circle me-2"></i>Salir
-                        </button>
-                      </li>
-                    </ul>
+                  <li className="nav-item">
+                    <a href="/" className="nav-link" aria-current="page">
+                      <i className="bi bi-house-heart me-2" />Inicio
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="/categorias" className="nav-link">
+                      <i className="bi bi-bookmark-star me-2" />Categorías
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="/productos" className="nav-link">
+                      <i className="bi bi-bag-heart me-2" />Productos
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="/pedidos" className="nav-link"><i className="bi bi-truck me-2" />Pedidos
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <button type='button' onClick={cerrarSesion} className="nav-link">
+                      <i class="bi bi-x-circle me-2"></i>Salir
+                    </button>
                   </li>
                 </ul>
               ) :
               (
                 <ul className="nav nav-pills">
                   <li className="nav-item">
-                    <a href="/productos/cliente" className="nav-link active" aria-current="page">
+                    <a href="/usuarios/form" className="nav-link active" aria-current="page">
+                      <i class="bi bi-person-square me-2"></i>{usuario.nombres}
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a href="/productos/cliente" className="nav-link" aria-current="page">
                       <i className="bi bi-house-heart me-2"></i>Catálogo
                     </a>
                   </li>
--- src/componentes/usuarios/Gestor.js
+++ src/componentes/usuarios/Gestor.js
@@ -1,7 +1,11 @@
 function Gestor() {
     return (
-        <div>
-            <h3>Gestor del aplicativo</h3>
+        <div className="mt-4 ms-5 me-5 mb-5">
+            <div class="card backGestor" >
+                <div class="card-body">
+                    <h3 class="card-title">Gestor del aplicativo</h3>
+                </div>
+            </div>
         </div>
     )
 }
